210e8627d61a619a502686b9ab768bcc7da35fb3
Ben Radler <ben@benradler.com>
2020-06-03 22:29:45 -0700

    Initial Commit

---
+++ swiftyninjaTests/swiftyninjaTests.swift
@@ -0,0 +1,34 @@
+//
+//  swiftyninjaTests.swift
+//  swiftyninjaTests
+//
+//  Created by iMac on 6/3/20.
+//  Copyright © 2020 Lord Nibbler. All rights reserved.
+//
+
+import XCTest
+@testable import swiftyninja
+
+class swiftyninjaTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
